a18b6baa8e6006004af777730ce204d7dc2fa513
Liam Kinne <kinneliam@gmail.com>
2017-02-24 11:46:12 +1100

    added address argument

--- src/host/roborio/C++/PixyI2C.h
+++ src/host/roborio/C++/PixyI2C.h
@@ -27,44 +27,44 @@
 
 #include "TPixy.h"
 
-#define PIXY_I2C_DEFAULT_ADDR  0x54
-#define PIXY_I2C_DEFAULT_PORT  I2C::Port::kOnboard
+#define PIXY_I2C_DEFAULT_ADDR	0x54
+#define PIXY_I2C_DEFAULT_PORT	frc::I2C::Port::kOnboard
 
 class LinkI2C
 {
 public:
-  LinkI2C(uint8_t address=PIXY_I2C_DEFAULT_ADDR, I2C::Port port):
-    Wire(port, address)
+  void SetArg(uint16_t arg)
   {
-
-  }
-  LinkI2C():
-    Wire(PIXY_I2C_DEFAULT_PORT, PIXY_I2C_DEFAULT_ADDR)
-  {
-
+  if (arg==PIXY_DEFAULT_ARGVAL)
+    addr = PIXY_I2C_DEFAULT_ADDR;
+  else
+    addr = arg;
   }
   uint16_t getWord()
   {
+  auto Wire = new frc::I2C(frc::I2C::Port::kOnboard, addr);
     uint8_t c[2];
-    Wire.ReadOnly(2, c);
+    Wire->ReadOnly(2, c);
     uint16_t w = (c[1] << 8) + c[0];
     return w;
   }
   uint8_t getByte()
   {
-    uint8_t *c;
-    Wire.ReadOnly(1, c);
-    return *c;
+  auto Wire = new frc::I2C(frc::I2C::Port::kOnboard, addr);
+    uint8_t c[1];
+    Wire->ReadOnly(1, c);
+    return c[0];
   }
 
   int8_t send(uint8_t *data, uint8_t len)
   {
-    Wire.WriteBulk(data, len);
+  auto Wire = new frc::I2C(frc::I2C::Port::kOnboard, addr);
+    Wire->WriteBulk(data, len);
     return len;
   }
   
 private:
-  I2C Wire;
+  uint16_t addr;
 };
 
 typedef TPixy<LinkI2C> PixyI2C;
